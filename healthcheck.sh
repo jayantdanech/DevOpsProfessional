95aee26d45a7785c48c92a13db91f5b95c2f4bbb
Jayant Danech <jayant.danech@gmail.com>
2025-11-13 13:55:27 +0530

    healthcheck.sh

---
+++ healthcheck.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Author:  Jayant Danech
+# Purpose:  Check the application status on port 85 first.
+
+URL="http://localhost:85"
+
+status=$(curl -s -o /dev/null -w "%{http_code}" $URL)
+
+if [ "$status" -eq 200 ]; then
+    echo "Health check passed. App is working."
+    exit 0
+else
+    echo "Health check FAILED. Returned status: $status"
+    exit 1
+fi
+
