2a9bc7f6c0a4b88abacd7a3f6c83c903e1651265
Jayant Danech <jayant.danech@gmail.com>
2025-11-13 14:47:26 +0530

    debug

--- healthcheck.sh
+++ healthcheck.sh
@@ -8,11 +8,13 @@ status=$(curl -s -o /dev/null -w "%{http_code}" $URL)
 
 if [ "$status" -eq 200 ]; then
     echo "Health check passed. App is working."
+    echo "curl $URL"
+    curl $URL
     exit 0
 else
     echo "Health check FAILED. Returned status: $status"
+    echo "curl $URL"
+    curl $URL
     exit 1
 fi
 
-echo "curl $URL"
-curl $URL
