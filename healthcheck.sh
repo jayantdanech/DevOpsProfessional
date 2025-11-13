d6ff7f652c979add0391bde53ce7ead61e016156
Jayant Danech <jayant.danech@gmail.com>
2025-11-13 14:50:10 +0530

    debug

--- healthcheck.sh
+++ healthcheck.sh
@@ -3,18 +3,20 @@
 # Purpose:  Check the application status on port 85 first.
 
 URL="http://localhost:85"
+debuginfo() {
+    echo "curl $URL"
+    curl $URL
+}
 
 status=$(curl -s -o /dev/null -w "%{http_code}" $URL)
 
 if [ "$status" -eq 200 ]; then
     echo "Health check passed. App is working."
-    echo "curl $URL"
-    curl $URL
+    debuginfo
     exit 0
 else
     echo "Health check FAILED. Returned status: $status"
-    echo "curl $URL"
-    curl $URL
+    debuginfo
     exit 1
 fi
 
